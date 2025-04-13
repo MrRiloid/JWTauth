a79b0bbd2549bd0aaec0b648cafd13eedaeceeae
Matvey <m5221211m@gmail.com>
2025-04-13 20:56:31 +0300

    init

---
+++ src/test/java/org/matvey/jwtauth/JwTauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matvey.jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
